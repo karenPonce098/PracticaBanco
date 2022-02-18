b141feea6cec573fddc9e79e1749362e7558a275
KarenPonce98 <thecasualites310@gmail.com>
2022-02-17 23:11:52 -0600

    Base2

--- spring-eshop-master/src/main/java/com/victommasi/eshop/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.victommasi.eshop.dao;
-
-import org.springframework.stereotype.Repository;
-import com.victommasi.eshop.model.User;
-
-@Repository
-public interface UserDao extends GenericDao<User, String>{
-	
-}
